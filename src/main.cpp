253d85b0ce3cbec9e168b8f649366e92988a904d
Chilop <jjuulliiooooooooo@gmail.com>
2025-04-25 08:22:30 -0600

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,71 @@
+/* Amerike university
+     Author: Julio López
+     Date: 11/04/25
+     Practice #: 22
+     Description: This program shall use constructors to show data*/
+ 
+     #include <iostream>
+     using namespace std;
+
+     class People
+     {
+     private:
+     string Name;
+     int Age;
+     double Height;
+
+     public:
+     
+     People(string NameX, int AgeX, double HeightX)
+     {
+        Name = NameX;
+        Age = AgeX;
+        Height = HeightX;
+     }
+     void setName(string Name1)
+     {
+        Name = Name1;
+     }
+     string getName()
+     {
+        return Name;
+     }
+     void setAge(int Age1)
+     {
+        Age = Age1;
+     }
+     int getAge()
+     {
+        return Age;
+     }
+     void setHeight(double Height1)
+     {
+        Height = Height1;
+     }
+     double getHeight()
+     {
+        return Height;
+     }
+    };
+     int main(){
+     People person1("Luis", 33, 1.77);
+     People person2("Ana", 25, 1.51);
+     People person3("Juan", 46, 1.83);
+
+     cout << person1.getName() << "\n";
+     cout << person1.getAge() << "\n";
+     cout << person1.getHeight() << "\n";
+     cout << "\n";
+
+     cout << person2.getName() << "\n";
+     cout << person2.getAge() << "\n";
+     cout << person2.getHeight() << "\n";
+     cout << "\n";
+
+     cout << person3.getName() << "\n";
+     cout << person3.getAge() << "\n";
+     cout << person3.getHeight() << "\n";
+     cout << "\n";
+
+     return 0;
+    }
